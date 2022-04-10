451bf3f77df85f63479c2a59f17c4c12ed3686a4
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-04-10 21:14:58 +0700

    Create uptime.js

---
+++ modules/commands/uptime.js
@@ -0,0 +1,20 @@
+module.exports.config = {
+  name: "uptime",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "ManhG",
+  description: "Kiểm tra thời gian bot đã online",
+  commandCategory: "system",
+  cooldowns: 5,
+  dependencies: {}
+};
+
+module.exports.run = async ({ api, event }) => {
+  const time = process.uptime(),
+    days = Math.floor(time / (3600 * 24)),
+    hours = Math.floor(time % (3600 * 24) / 3600),
+    minutes = Math.floor((time / 60) % 60),
+    seconds = Math.floor(time % 60);
+
+  return api.sendMessage([days, hours, minutes, seconds].map(v => (v < 10 ? "0" + v : v)).filter((v, i) => v !== "00" || i > 0).join(":"), event.threadID, event.messageID);
+}
