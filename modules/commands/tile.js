81598be22bf73fed3669fc903db8877eeefaad4d
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-04-30 14:16:20 +0700

    Update tile.js

--- modules/commands/tile.js
+++ modules/commands/tile.js
@@ -26,10 +26,10 @@ module.exports.config = {
   var arraytag=[];arraytag["push"]({id:mention,tag:name});arraytag["push"]({id:event["senderID"],tag:namee});
   var mentions=Object["keys"](event["mentions"]);
 
-  let Avatar=( await axios["get"](`${"https://graph.facebook.com/"}${mentions}${"/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662"}`,{responseType:"arraybuffer"}))["data"];
+  let Avatar=( await axios["get"](`${"https://graph.facebook.com/"}${mentions}${"/picture?height=1500&width=1500&access_token=1073911769817594|aa417da57f9e260d1ac1ec4530b417de"}`,{responseType:"arraybuffer"}))["data"];
   fs["writeFileSync"](__dirname+ "/cache/avt.png",Buffer["from"](Avatar,"utf-8"));
 
-  let Avatar2=( await axios["get"](`${"https://graph.facebook.com/"}${event["senderID"]}${"/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662"}`,{responseType:"arraybuffer"}))["data"];
+  let Avatar2=( await axios["get"](`${"https://graph.facebook.com/"}${event["senderID"]}${"/picture?height=1500&width=1500&access_token=1073911769817594|aa417da57f9e260d1ac1ec4530b417de"}`,{responseType:"arraybuffer"}))["data"];
 
   fs["writeFileSync"](__dirname+ "/cache/avt2.png",Buffer["from"](Avatar2,"utf-8"));
   
