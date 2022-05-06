cdd5a0895c2b275963e8e85ecc2f55614056105a
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-05-07 03:21:17 +0700

    Update ad.js

--- modules/commands/ad.js
+++ modules/commands/ad.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "ad",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "JRT",
+  credits: "JRT",// mod api do ai đó=))
   description: "Kiểm tra thông tin admin bot.",
   commandCategory: "Thông tin adminbot",
   usages: "adm",
@@ -18,10 +18,13 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-  var link = [
-  "https://i.imgur.com/OOzLYYB.jpg"
-  ];
-  var callback = () => api.sendMessage({body:`★ Admin BOT ★
- 🐧Tên: Nguyễn Gia Khang\n🎂Ngày sinh: 18/10/200?\n😎Chiều cao x cân nặng: 1m8 x 38kg\n💒Quê quán: Bình Định\n🏢Nơi sống: Bà Rịa Vũng Tàu\n📢Sở thích: Ngắm gái, hút thuốc, chơi game,...\n😶‍🌫️Tính cách: bth`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg")); 
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
+  const res = await axios.get("https://api.xlshsad.repl.co/images/mirai");
+//lấy data trên web api
+const data = res.data.url;
+//tải ảnh xuống
+let download = (await axios.get(data, {
+			responseType: "stream"
+		})).data;
+return api.sendMessage({body:`★ Admin BOT ★
+ 🐧Tên: Nguyễn Gia Khang\n🎂Ngày sinh: 18/10/200?\n😎Chiều cao x cân nặng: 1m8 x 38kg\n💒Quê quán: Bình Định\n🏢Nơi sống: Bà Rịa Vũng Tàu\n📢Sở thích: Ngắm gái, chơi game,...\n😶‍🌫️Tính cách: bth`,attachment: download}, event.threadID, event.messageID);
    };
