f9c28b2125df5fcf7eac556ba1ebca9700409fdc
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-05-17 22:34:58 +0700

    Update joinNoti.js

--- modules/events/joinNoti.js
+++ modules/events/joinNoti.js
@@ -2,28 +2,45 @@ module.exports.config = {
 	name: "joinNoti",
 	eventType: ["log:subscribe"],
 	version: "1.0.1",
-	credits: "Mirai Team",
-	description: "Thông báo bot hoặc người vào nhóm",
+	credits: "CatalizCS",
+	description: "Thông báo bot hoặc người vào nhóm có random gif/ảnh/video",
 	dependencies: {
-		"fs-extra": ""
+		"fs-extra": "",
+		"path": "",
+		"pidusage": ""
 	}
 };
+
+module.exports.onLoad = function () {
+    const { existsSync , mkdirSync } = global.nodemodule["fs-extra"];
+    const { join } = global.nodemodule["path"];
+
+	const path = join(__dirname, "cache", "joinMp4");
+	if (existsSync(path)) mkdirSync(path, { recursive: true });	
+
+	const path2 = join(__dirname, "cache", "joinGif", "randomgif");
+    if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
+
+    return;
+}
+
 
 module.exports.run = async function({ api, event }) {
 	const { join } = global.nodemodule["path"];
 	const { threadID } = event;
 	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-		api.changeNickname(`👉 ${global.config.PREFIX} 👈 💖 ${(!global.config.BOTNAME) ? "ChatBot Cre By GK" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
-		return api.sendMessage(`⫸ 𝐊𝐞̂́𝐭 𝐍𝐨̂́𝐢 𝐓𝐡𝐚̀𝐧𝐡 𝐂𝐨̂𝐧𝐠 ⫷`, threadID);
+		api.changeNickname(`👉 ${global.config.PREFIX} 👈 🤍 ${(!global.config.BOTNAME) ? "Cre by CatalizCS" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+		const fs = require("fs");
+		return api.sendMessage("", event.threadID, () => api.sendMessage({body:`⫸ 𝐊𝐞̂́𝐭 𝐍𝐨̂́𝐢 𝐓𝐡𝐚̀𝐧𝐡 𝐂𝐨̂𝐧𝐠 ⫷`, attachment: fs.createReadStream(__dirname + "/cache/joinMp4/hello.gif")} ,threadID));
 	}
 	else {
 		try {
-			const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+			const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
 			let { threadName, participantIDs } = await api.getThreadInfo(threadID);
 
 			const threadData = global.data.threadData.get(parseInt(threadID)) || {};
 			const path = join(__dirname, "cache", "joinMp4");
-			const pathGif = join(path,`hi.mp4`);
+			const pathGif = join(path, `${threadID}.mp4`);
 
 			var mentions = [], nameArray = [], memLength = [], i = 0;
 			
@@ -35,19 +52,25 @@ module.exports.run = async function({ api, event }) {
 			}
 			memLength.sort((a, b) => a - b);
 			
-			(typeof threadData.customJoin == "undefined") ? msg = "🇻🇳 𝑾𝒆𝒍𝒄𝒐𝒎𝒆 {name} \n🔰Đ𝒂̃ đ𝒆̂́𝒏 𝒗𝒐̛́𝒊 𝒃𝒐𝒙 {threadName}\n👤𝑩𝒂̣𝒏 𝒍𝒂̀ 𝒕𝒉𝒂̀𝒏𝒉 𝒗𝒊𝒆̂𝒏 𝒕𝒉𝒖̛́  {soThanhVien} \n⚡𝑽𝒖𝒊 𝒍𝒐̀𝒏𝒈 đ𝒐̣𝒄 𝒏𝒉𝒖̛̃𝒏𝒈 đ𝒊𝒆̂̀𝒖 𝒍𝒆̣̂𝒏𝒉 𝒔𝒂𝒖 đ𝒂̂𝒚 𝒏𝒉𝒆́ \n📌𝑫𝒖̀𝒏𝒈 𝒍𝒆̣̂𝒏𝒉 ,𝒔𝒆𝒕𝒏𝒂𝒎𝒆 + 𝑻𝒆̂𝒏 𝒄𝒂̂̀𝒏 đ𝒐̂̉𝒊 \n💞𝑮𝒊𝒐̛́𝒊 𝒕𝒉𝒊𝒆̣̂𝒖 𝑯𝒐̣ 𝒕𝒆̂𝒏... \n👼𝑵𝒂̆𝒎 𝒔𝒊𝒏𝒉... \n🏘️𝑵𝒐̛𝒊 𝒐̛̉... \n💟𝑻𝒖̛̀ 𝒈𝒊𝒐̛̀ 𝒃𝒂̣𝒏 𝒔𝒆̃ 𝒍𝒂̀ 𝒎𝒐̣̂𝒕 𝒕𝒉𝒂̀𝒏𝒉 𝒗𝒊𝒆̂𝒏 𝒄𝒖̉𝒂 𝒃𝒐𝒙 𝒎𝒊̀𝒏𝒉 💖♥" : msg = threadData.customJoin;
+			(typeof threadData.customJoin == "undefined") ? msg = `🇻🇳 𝑾𝒆𝒍𝒄𝒐𝒎𝒆 {name} \n🔰Đ𝒂̃ đ𝒆̂́𝒏 𝒗𝒐̛́𝒊 𝒃𝒐𝒙 {threadName}\n👤𝑩𝒂̣𝒏 𝒍𝒂̀ 𝒕𝒉𝒂̀𝒏𝒉 𝒗𝒊𝒆̂𝒏 𝒕𝒉𝒖̛́  {soThanhVien} \n⚡𝑽𝒖𝒊 𝒍𝒐̀𝒏𝒈 đ𝒐̣𝒄 𝒏𝒉𝒖̛̃𝒏𝒈 đ𝒊𝒆̂̀𝒖 𝒍𝒆̣̂𝒏𝒉 𝒔𝒂𝒖 đ𝒂̂𝒚 𝒏𝒉𝒆́ \n📌𝑫𝒖̀𝒏𝒈 𝒍𝒆̣̂𝒏𝒉 ${global.config.PREFIX}𝒔𝒆𝒕𝒏𝒂𝒎𝒆 + 𝑻𝒆̂𝒏 𝒄𝒂̂̀𝒏 đ𝒐̂̉𝒊 \n💞𝑮𝒊𝒐̛́𝒊 𝒕𝒉𝒊𝒆̣̂𝒖 𝑯𝒐̣ 𝒕𝒆̂𝒏... \n👼𝑵𝒂̆𝒎 𝒔𝒊𝒏𝒉... \n🏘️𝑵𝒐̛𝒊 𝒐̛̉... \n💟𝑻𝒖̛̀ 𝒈𝒊𝒐̛̀ 𝒃𝒂̣𝒏 𝒔𝒆̃ 𝒍𝒂̀ 𝒎𝒐̣̂𝒕 𝒕𝒉𝒂̀𝒏𝒉 𝒗𝒊𝒆̂𝒏 𝒄𝒖̉𝒂 𝒃𝒐𝒙 𝒎𝒊̀𝒏𝒉 💖♥` : msg = threadData.customJoin;
 			msg = msg
 			.replace(/\{name}/g, nameArray.join(', '))
-			.replace(/\{type}/g, (memLength.length > 1) ?  'các bạn' : 'bạn')
+			.replace(/\{type}/g, (memLength.length > 1) ?  '𝐂𝐚́𝐜 𝐁𝐚̣𝐧' : '𝐁𝐚̣𝐧')
 			.replace(/\{soThanhVien}/g, memLength.join(', '))
 			.replace(/\{threadName}/g, threadName);
 
 			if (existsSync(path)) mkdirSync(path, { recursive: true });
 
+			const randomPath = readdirSync(join(__dirname, "cache", "joinMp4" ));
+
 			if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
+			else if (randomPath.length != 0) {
+				const pathRandom = join(__dirname, "cache", "joinMp4", `${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
+				formPush = { body: msg, attachment: createReadStream(pathRandom), mentions }
+			}
 			else formPush = { body: msg, mentions }
 
 			return api.sendMessage(formPush, threadID);
 		} catch (e) { return console.log(e) };
 	}
-        }
+}
