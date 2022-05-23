0b921ac4d6b147ace92e685e246f8bfe3fe272fd
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-05-24 02:02:08 +0700

    Update uid.js

--- modules/commands/uid.js
+++ modules/commands/uid.js
@@ -2,13 +2,17 @@ module.exports.config = {
 	name: "uid",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "Mirai Team",
+	credits: "Mirai Team",// mod reply by NguyenKhang
 	description: "Lấy ID người dùng.",
 	commandCategory: "other",
 	cooldowns: 5
 };
 
 module.exports.run = function({ api, event }) {
+  if (event.type == "message_reply") {
+    id = event.messageReply.senderID
+    return api.sendMessage(id,event.threadID, event.messageID)
+  }
 	if (Object.keys(event.mentions) == 0) return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
 	else {
 		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
