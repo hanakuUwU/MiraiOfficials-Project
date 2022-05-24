d0dfdf5ba88ff4da0998c366806a52525655953c
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-05-24 22:44:48 +0700

    Update upt.js

--- modules/commands/upt.js
+++ modules/commands/upt.js
@@ -1,40 +1,39 @@
+const fbname = "Hanaku.user";
+const githubname = "HanakuUwU";
 module.exports.config = {
-	name: "upt",
-	version: "1.0.1",
+	name:"upt",
+	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "Mirai Team",// Đã mod thêm thời gian và ngày
-	description: "Kiểm tra thời gian bot đã online",
-	commandCategory: "Tiện ích",
-	cooldowns: 5,
-	dependencies: {
-		"pidusage": "",
-    "fast-speedtest-api": ""
+	credits: "Mirai-Team",
+	description: "Random ảnh theo api - uptime",
+	commandCategory: "system",
+	cooldowns: 3,
+  dependencies: {
+		"pidusage": ""
 	}
 };
-
 function byte2mb(bytes) {
 	const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 	let l = 0, n = parseInt(bytes, 10) || 0;
 	while (n >= 1024 && ++l) n = n / 1024;
 	return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
 }
-
 module.exports.run = async ({ api, event, args }) => {
-		const fast = global.nodemodule["fast-speedtest-api"];
-   const prefix = config.PREFIX
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+  var z_1 = (hours < 10) ? '0' + hours : hours;
+    var x_1 = (minutes < 10) ? '0' + minutes : minutes;
+    var y_1 = (seconds < 10) ? '0' + seconds : seconds;
+    const axios = require('axios')
+	const pidusage = await global.nodemodule["pidusage"](process.pid);
+	const timeStart = Date.now();
+  const fs = require('fs-extra');
   const moment = require("moment-timezone");
-	const axios = global.nodemodule["axios"];
-		const speedTest = new fast({
-			token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
-			verbose: false,
-			timeout: 10000,
-			https: true,
-			urlCount: 5,
-			bufferSize: 8,
-			unit: fast.UNITS.Mbps
-		});
-  var gio = moment.tz("Asia/Ho_Chi_Minh").format("D/MM/YYYY || HH:mm:ss");
-   var thu = moment.tz('Asia/Ho_Chi_Minh').format('dddd');
+  var gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss");
+  var cc = moment.tz('Asia/Ho_Chi_Minh').format('D/MM/YYYY');
+    var thu = moment.tz('Asia/Ho_Chi_Minh').format('dddd');
   if (thu == 'Sunday') thu = '𝐂𝐡𝐮̉ 𝐍𝐡𝐚̣̂𝐭'
   if (thu == 'Monday') thu = '𝐓𝐡𝐮̛́ 𝟐'
   if (thu == 'Tuesday') thu = '𝐓𝐡𝐮̛́ 𝟑'
@@ -42,19 +41,109 @@ module.exports.run = async ({ api, event, args }) => {
   if (thu == "Thursday") thu = '𝐓𝐡𝐮̛́ 𝟓'
   if (thu == 'Friday') thu = '𝐓𝐡𝐮̛́ 𝟔'
   if (thu == 'Saturday') thu = '𝐓𝐡𝐮̛́ 𝟕'
-    const res = await axios.get("https://api.xlshsad.repl.co/images/mirai");
-//lấy data trên web api
-const data = res.data.url;
-//tải ảnh xuống
-let download = (await axios.get(data, {
-			responseType: "stream"
-		})).data;
-		const resault = await speedTest.getSpeed();
-	const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
-	const pidusage = await global.nodemodule["pidusage"](process.pid);
-	const timeStart = Date.now();
-	return api.sendMessage("", event.threadID, () => api.sendMessage({body: `❯𝐇𝐨̂𝐦 𝐧𝐚𝐲 𝐥𝐚̀: ${thu}\n❯${gio}\n❯𝐓𝐢𝐦𝐞: ${hours} 𝐆𝐢𝐨̛̀ ${minutes} 𝐏𝐡𝐮́𝐭 ${seconds} 𝐆𝐢𝐚̂𝐲\n❯𝐂𝐩𝐮: ${pidusage.cpu.toFixed(1)}%\n❯𝐑𝐚𝐦: ${byte2mb(pidusage.memory)}\n❯𝐔𝐬𝐞𝐫𝐬: ${global.data.allUserID.length}\n❯𝐏𝐢𝐧𝐠: ${Date.now() - timeStart}𝐦𝐬\n❯𝐅𝐚𝐬𝐭: ${resault} 𝐌𝐛𝐬`, attachment: download}, event.threadID, event.messageID));
+   if (!fs.existsSync(__dirname +
+        `/tad/UTM-Avo.ttf`)) {
+        let getfont = (await axios.get(`https://github.com/hanakuUwU/font/raw/main/UTM%20Avo.ttf`, { responseType: "arraybuffer" })).data;
+        fs.writeFileSync(__dirname + `/tad/UTM-Avo.ttf`, Buffer.from(getfont, "utf-8"));
+      }
+         if (!fs.existsSync(__dirname +
+      `/tad/phenomicon.ttf`)) {
+      let getfont2 = (await axios.get(`https://github.com/hanakuUwU/font/raw/main/phenomicon.ttf`, { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(__dirname + `/tad/phenomicon.ttf`, Buffer.from(getfont2, "utf-8"));
+    };
+  if (!fs.existsSync(__dirname +
+      `/tad/CaviarDreams.ttf`)) {
+      let getfont3 = (await axios.get(`https://github.com/hanakuUwU/font/raw/main/CaviarDreams.ttf`, { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(__dirname + `/tad/CaviarDreams.ttf`, Buffer.from(getfont3, "utf-8"));
+    };
+   const { loadImage, createCanvas, registerFont } = require("canvas");
+  
+  let k = args[0];
+   if(args[0] == "list"){
+    const alime = (await axios.get('https://run.mocky.io/v3/6aa59c3e-ff9f-41cd-8611-07a1b870042d')).data
+    var count = alime.listAnime.length;
+      var data = alime.listAnime
+      var page = 1;
+      page = parseInt(args[1]) || 1;
+      page < -1 ? page = 1 : "";
+      var limit = 20;
+      var numPage = Math.ceil(count/limit);
+      var msg = ``;
+      for(var i = limit*(page - 1); i < limit*(page-1) + limit; i++){
+         if(i >= count) break;
+        msg += `[ ${i+1} ] - ${data[i].ID} | ${data[i].name}\n`;
+      }
+      msg += `Trang (${page}/${numPage})\nDùng ${global.config.PREFIX}${this.config.name} list <số trang>`;
+      return api.sendMessage(msg, event.threadID,event.messageID);
+   }
+  if(!k){
+  var id = Math.floor(Math.random() * 848) +1
+  } else {
+    var id = k
+  }
+    const lengthchar = (await axios.get('https://run.mocky.io/v3/0dcc2ccb-b5bd-45e7-ab57-5dbf9db17864')).data
+    console.log(lengthchar.length)
+  const Canvas = require('canvas');
+    let pathImg = __dirname + `/tad/avatar_1111231.png`;
+    let pathAva = __dirname + `/tad/avatar_3dsc11.png`;
+    let background = (await axios.get(encodeURI(`https://imgur.com/x5JpRYu.png`), { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(pathImg, Buffer.from(background, "utf-8"));
+    let ava = (await axios.get(encodeURI(`${lengthchar[id - 1].imgAnime}`), { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(pathAva, Buffer.from(ava, "utf-8"));
+    const request = require('request');
+    const path = require('path');
+
+  //const a = Math.floor(Math.random() * 820) + 1
+  
+  
+let l1 = await loadImage(pathAva);
+    let a = await loadImage(pathImg);
+    let canvas = createCanvas(a.width, a.height);
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = lengthchar[id - 1].colorBg;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(l1, -200, -200, 1200, 1200);
+    ctx.drawImage(a, 0, 0, canvas.width, canvas.height);
+     registerFont(__dirname + `/tad/phenomicon.ttf`, {
+      family: "phenomicon"
+    });
+    ctx.textAlign = "start";
+    ctx.strokeStyle = lengthchar[id - 1].colorBg;
+    ctx.filter = "brightness(90%) contrast(110%)";
+    ctx.font = "130px phenomicon";
+    ctx.fillStyle = lengthchar[id].colorBg;
+    ctx.fillText(global.config.BOTNAME, 900, 340);
+    ctx.beginPath();
+  ////////////////////////////////////////
+   registerFont(__dirname + `/tad/UTM-Avo.ttf`, {
+      family: "UTM"
+    });
+    ctx.textAlign = "start";
+    ctx.font = "70px UTM";
+    ctx.fillStyle = "#000000";
+    ctx.fillText(`${z_1} : ${x_1} : ${y_1} `, 920, 440);
+    ctx.restore();
+    ctx.save();
+registerFont(__dirname + `/tad/CaviarDreams.ttf`, {
+      family: "time"
+    });
+    ctx.textAlign = "start";
+    ctx.font = "45px time";
+    ctx.fillText("@" + " Anh", 930, 540)
+    ctx.fillText("@" + " Yêu", 930, 610)
+    ctx.fillText("@" + " Em", 930, 690)
+    ctx.restore();
+    ctx.save();
+    ctx.beginPath();
+    const imageBuffer = canvas.toBuffer();
+   fs.writeFileSync(pathImg, imageBuffer);
+  return api.sendMessage({
+    body: `Bot đã hoạt động được ${hours} giờ ${minutes} phút ${seconds} giây.\n\n❯ Hôm nay là ${thu} || ${cc}\n❯ Tổng người dùng: ${global.data.allUserID.length}\n❯ Tổng Nhóm: ${global.data.allThreadID.length}\n❯ Verrsion: ${global.config.version}\n❯ Cpu đang sử dụng: ${pidusage.cpu.toFixed(1)}%\n❯ Ram đang sử dụng: ${byte2mb(pidusage.memory)}\n❯ Ping: ${Date.now() - timeStart}ms\n❯ ID Nhân Vật: ${id}\n⠀⠀⠀⠀⠀⠀『${gio}』`,
+    attachment: fs.createReadStream(pathImg)
+  },
+    event.threadID,
+    () => fs.unlinkSync(pathImg),
+    fs.unlinkSync(pathAva),
+    event.messageID
+  );
 }
