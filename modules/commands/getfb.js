c662b911cd16e1105e987ce723a785d4583c91c9
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-05-24 11:52:31 +0700

    Update getfb.js

--- modules/commands/getfb.js
+++ modules/commands/getfb.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "getfb",
   versions: "1.0.0",
   hasPermssion: 0,
-  credits: "MiraiTeam",// mod reply by NguyenKhang
+  credits: "Mirai Team",// mod by NguyenKhang
   description: "Lấy link fb",
   commandCategory: "tiện ích",
   usages: "[Trống] or [Tag]/[reply]",
@@ -11,11 +11,11 @@ module.exports.config = {
 module.exports.run = function({ api, event }) {
   if (event.type == "message_reply") {
     id = event.messageReply.senderID
-    return api.sendMessage(`https://facebook.com/` + id,event.threadID, event.messageID)
+    return api.sendMessage(`facebook.com/` + id,event.threadID, event.messageID)
   }
-	if (Object.keys(event.mentions) == 0) return api.sendMessage(`https://facebook.com/${event.senderID}`, event.threadID, event.messageID);
+	if (Object.keys(event.mentions) == 0) return api.sendMessage(`facebook.com/${event.senderID}`, event.threadID, event.messageID);
   else {
-		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`https://facebook.com/${Object.keys(event.mentions)[i]}`, event.threadID, event.messageID);
+		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`facebook.com/${Object.keys(event.mentions)[i]}`, event.threadID, event.messageID);
 		return;
   }
 }
