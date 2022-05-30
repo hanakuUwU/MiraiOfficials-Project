8783db2398aed5cf20bc6019e0d45793d9fdebb9
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-05-30 11:20:20 +0700

    Update imgur.js

--- modules/commands/imgur.js
+++ modules/commands/imgur.js
@@ -1,4 +1,3 @@
-
 module.exports.config = {
     name: "imgur",
     version: "1.0.0",
@@ -9,15 +8,15 @@ module.exports.config = {
     usages: "[reply]",
     cooldowns: 5,
     dependencies: {
-  "axios": "",}
+      "axios": ""
+    }
 };
 
 module.exports.run = async ({ api, event }) => {
 const axios = global.nodemodule['axios'];  
 var linkanh = event.messageReply.attachments[0].url || args.join(" ");
     if(!linkanh) return api.sendMessage('Vui lòng reply hoặc nhập link 1 hình ảnh!!!', event.threadID, event.messageID)
-const res = await axios.get(`https://api.tvl0912hcmcity.repl.co/imgur?link=${encodeURIComponent(linkanh)}`);    
+const res = await axios.get(`https://API-Web.miraiofficials123.repl.co/imgur?link=${encodeURIComponent(linkanh)}`);    
 var img = res.data.uploaded.image;
     return api.sendMessage(`${img}`, event.threadID, event.messageID);
-    
   }
