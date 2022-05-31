8d102c81b1ce0912f8a8a4affe1ea1fe83c500be
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-01 00:03:08 +0700

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -5,9 +5,165 @@ module.exports = function({ api, models }) {
 				Currencies = require("./controllers/currencies")({ models });
 	const logger = require("../utils/log.js");
 	const fs = require("fs");
-	const moment = require('moment-timezone');
-	const axios = require("axios");
 
+    const moment = require('moment-timezone');
+
+    const axios = require("axios");
+
+  var day = moment.tz("Asia/Ho_Chi_Minh").day();
+
+  const checkttDataPath = __dirname + '/../modules/commands/tt/';
+
+  setInterval(async() => {
+
+    const day_now = moment.tz("Asia/Ho_Chi_Minh").day();
+
+    if (day != day_now) {
+
+      day = day_now;
+
+      const checkttData = fs.readdirSync(checkttDataPath);
+
+      console.log('--> CHECKTT: Ngày Mới');
+
+      checkttData.forEach(async(checkttFile) => {
+
+        const checktt = JSON.parse(fs.readFileSync(checkttDataPath + checkttFile));
+
+        let storage = [], count = 1;
+
+        for (const item of checktt.day) {
+
+            const userName = await Users.getNameUser(item.id) || 'Facebook User';
+
+            const itemToPush = item;
+
+            itemToPush.name = userName;
+
+            storage.push(itemToPush);
+
+        };
+
+        storage.sort((a, b) => {
+
+            if (a.count > b.count) {
+
+                return -1;
+
+            }
+
+            else if (a.count < b.count) {
+
+                return 1;
+
+            } else {
+
+                return a.name.localeCompare(b.name);
+
+            }
+
+        });
+
+        let checkttBody = '===Top 10 Tương Tác Ngày===\n';
+
+        checkttBody += storage.slice(0, 10).map(item => {
+
+          return `${count++}. ${item.name} (${item.count})`;
+
+      }).join('\n');
+
+        api.sendMessage(checkttBody, checkttFile.replace('.json', ''), (err) => err ? console.log(err) : '');
+
+        
+
+        checktt.day.forEach(e => {
+
+            e.count = 0;
+
+        });
+
+        checktt.time = day_now;
+
+        
+
+        fs.writeFileSync(checkttDataPath + checkttFile, JSON.stringify(checktt, null, 4));
+
+      });
+
+      if (day_now == 1) {
+
+        console.log('--> CHECKTT: Tuần Mới');
+
+        checkttData.forEach(async(checkttFile) => {
+
+          const checktt = JSON.parse(fs.readFileSync(checkttDataPath + checkttFile));
+
+          let storage = [], count = 1;
+
+          for (const item of checktt.week) {
+
+              const userName = await Users.getNameUser(item.id) || 'Facebook User';
+
+              const itemToPush = item;
+
+              itemToPush.name = userName;
+
+              storage.push(itemToPush);
+
+          };
+
+          storage.sort((a, b) => {
+
+              if (a.count > b.count) {
+
+                  return -1;
+
+              }
+
+              else if (a.count < b.count) {
+
+                  return 1;
+
+              } else {
+
+                  return a.name.localeCompare(b.name);
+
+              }
+
+          });
+
+          let checkttBody = '===Top 10 Tương Tác Tuần===\n';
+
+          checkttBody += storage.slice(0, 10).map(item => {
+
+            return `${count++}. ${item.name} (${item.count})`;
+
+        }).join('\n');
+
+          api.sendMessage(checkttBody, checkttFile.replace('.json', ''), (err) => err ? console.log(err) : '');
+
+          checktt.week.forEach(e => {
+
+              e.count = 0;
+
+          });
+
+          
+
+          fs.writeFileSync(checkttDataPath + checkttFile, JSON.stringify(checktt, null, 4));
+
+        })
+
+      }
+
+      global.client.sending_top = false;
+
+    }
+
+  }, 1000 * 10);
+
+
+  
 	//////////////////////////////////////////////////////////////////////
 	//========= Push all variable from database to environment =========//
 	//////////////////////////////////////////////////////////////////////
@@ -109,11 +265,10 @@ module.exports = function({ api, models }) {
 		timeMs = yearToMS + monthToMS + dayToMS + hourToMS + minuteToMS + secondToMS - oneDayToMS;
 		resolve(timeMs);
 	});
-
-
+  
 	const tenMinutes = 10 * 60 * 1000;
-
-		const checkAndExecuteEvent = async () => {
+  
+	const checkAndExecuteEvent = async () => {
 
 		/*smol check*/
 		if (!fs.existsSync(datlichPath)) fs.writeFileSync(datlichPath, JSON.stringify({}, null, 4));
