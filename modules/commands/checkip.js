707e1228af8ba8c0a6e7325ff2662815bfc926d0
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-07 03:31:36 +0700

    Update checkip.js

--- modules/commands/checkip.js
+++ modules/commands/checkip.js
@@ -1,6 +1,6 @@
 module.exports.config = {
   name: "checkip",
-  version: "1.2.0",
+  version: "1.0.0",
   hasPermssion: 0,
   credits: "K có chủ",// fix by Nguyen
   description: "lấy thông tin sever",
@@ -18,16 +18,11 @@ module.exports.run = async ({ api, event, args }) => {
   const ip = res.data.ip;
   const port = res.data.port;
   const host = res.data.hostname;
-  const ng = res.data.players.online;
-  const max = res.data.players.max;
-  return api.sendMessage({body: 
-    `= MÁY CHỦ MINECRAFT JAVA =` +
-    `\n\nHostName: ` + host +
-    `\nTrạng Thái: ` + onl +
-    `\nĐịa chỉ IP: ` + ip +
-    `\nPort: ` + port +
-    `\nSố người online: ` + ng +
-    `\nMaxPlayers: ` + max +
-    `\n\nNếu như sever off thì 1 số thứ sẽ ra underfind nhé chứ không phải lỗi đâu`
-     }, event.threadID, event.messageID)
+    return api.sendMessage({body: 
+    `= MÁY CHỦ MINECRAFT JAVA =` +
+    `\nHostName: ` + host +
+    `\nTrạng Thái: ` + onl +
+    `\nĐịa chỉ IP: ` + ip +
+    `\nPort: ` + port
+    }, event.threadID, event.messageID)
 }
