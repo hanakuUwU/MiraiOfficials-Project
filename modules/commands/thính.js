a0d18931d6d856584213caca6584f3846afc6e0d
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-06 20:35:11 +0700

    Update thính.js

--- modules/commands/thính.js
+++ modules/commands/thính.js
@@ -1,6 +1,6 @@
 module.exports.config = {
   name: "thính",
-  version: "1.2.0",
+  version: "1.4.0",
   hasPermssion: 0,
   credits: "Nguyen",
   description: "Xem ca dao Việt Nam",
@@ -13,8 +13,7 @@ module.exports.run = async ({ api, event }) => {
   const axios = require('axios');
   const res = await axios.get('https://APIThinh.miraiofficials123.repl.co');
   const data = res.data.data;
-  const qq = await axios.get(`https://APIURL.MIRAIOFFICIALS123.REPL.CO`);
-  const data2 = qq.data.url;
-  let cc = (await axios.get(data2, {			responseType: "stream"})).data;
+  const data2 = res.data.url;
+  let cc = (await axios.get(data2, {responseType: "stream"})).data;
   return api.sendMessage({body: `Thính: `+data, attachment: cc}, event.threadID, event.messageID)
 }
