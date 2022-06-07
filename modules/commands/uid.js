b59af3ee7576b97042ea72fe0595bdfe92549700
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-07 12:30:17 +0700

    Update uid.js

--- modules/commands/uid.js
+++ modules/commands/uid.js
@@ -1,22 +1,22 @@
 module.exports.config = {
 	name: "uid",
-	version: "1.0.0",
+	version: "1.2.0",
 	hasPermssion: 0,
-	credits: "Mirai Team",
+	credits: "Nguyen",
 	description: "Lấy ID người dùng.",
 	commandCategory: "other",
 	cooldowns: 5
-};
-
-module.exports.run = function({ api, event }) {
-  const { threadID, messageID, mentions, type, senderID, messageReply  } = event;
-  if(type == "message_reply"){ 
-    id = messageReply.senderID
-    return api.sendMessage(id, threadID, messageID)
 }
-	if (Object.keys(mentions) == 0) return api.sendMessage(`${senderID}`, threadID, messageID);
-	else {
-		for (var i = 0; i < Object.keys(mentions).length; i++) api.sendMessage(`${Object.values(mentions)[i].replace('@', '')}: ${Object.keys(mentions)[i]}`, threadID);
-		return;
-	}
+
+module.exports.run = async function ({ api, event, Users, args }) {
+  const { type, threadID, messageID, mentions, senderID, messageReply } = event;
+  if (type == "message_reply") {
+      uid = messageReply.senderID
+    } else if (args.join().indexOf('@') !== -1) {
+        var uid = Object.keys(mentions)[0]
+    }  else {
+        var uid = senderID
+  } 
+  var name = await Users.getNameUser(uid)
+    return api.sendMessage(name + `: ` + uid, threadID, messageID);
 }
