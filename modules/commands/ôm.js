2f3fbb9b5c4324463e4ce3add096ceb60cd34d34
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-10 21:45:54 +0700

    Update ôm.js

--- modules/commands/ôm.js
+++ modules/commands/ôm.js
@@ -15,13 +15,13 @@ module.exports.config = {
   }
 }
 
-module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+module.exports.run = async({api,event}) => {
                   var mention = Object.keys(event.mentions)[0];
 let tag = event.mentions[mention].replace("@", "");
         var link = [
           "https://i.imgur.com/HvJmLSI.gif",
              ];
-   var callback = () => api.sendMessage({body: `${name} à 💌, 𝐈 𝐥𝐨𝐯𝐞 𝐘𝐨𝐮 💗` , 
+   var callback = () => api.sendMessage({body: `${tag} à 💌, 𝐈 𝐥𝐨𝐯𝐞 𝐘𝐨𝐮 💗` , 
   attachment: fs.createReadStream(__dirname + "/cache/omngu.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/omngu.gif"));
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/omngu.gif")).on("close",() => callback());
 }
