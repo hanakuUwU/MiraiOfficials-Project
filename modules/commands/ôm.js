e3e137cd8c8a888271ef31677e20db1fe29f483a
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-10 20:24:10 +0700

    Update ôm.js

--- modules/commands/ôm.js
+++ modules/commands/ôm.js
@@ -1,27 +1,36 @@
 const request = require("request");
-const fs = require("fs")
-const axios = require("axios")
+const fs = require("fs-extra");
 module.exports.config = {
   name: "ôm",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "Lê Định Mod",
+  credits: "Lê Định mod thêm by TrúcCute",
   description: "ôm người Bạn Muốn",
   commandCategory: "Tình yêu",
-  usages: "@tag",
+  usages: "[id/tag/reply]",
   cooldowns: 5,
-  dependencies: {"request": "","fs": "","axios": ""}
-};
+  dependencies: {
+    "request": "",
+    "fs-extra": ""
+  }
+}
 
-module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
-        const request = require('request')
-                const fs = require('fs')
-                  var mention = Object.keys(event.mentions)[0];
-let tag = event.mentions[mention].replace("@", "");
+module.exports.run = async ({ api, event, Users, args }) => {
+  const { threadID, messageID, senderID, type, mentions, messageReply } = event;
+   if (type == "message_reply") {
+      uid = messageReply.senderID
+   } else if (args.join().indexOf('@') !== -1) {
+        var uid = Object.keys(mentions)[0]
+   } else if (args.join().indexOf('1000') !== -1) {
+        uid = `${args[0]}`
+   } else {
+     var uid = senderID
+   }
+    let name = await Users.getNameUser(uid)
         var link = [
           "https://i.imgur.com/HvJmLSI.gif",
              ];
-   var callback = () => api.sendMessage({body: `${tag} à 💌, 𝐈 𝐥𝐨𝐯𝐞 𝐘𝐨𝐮 💗` , 
+   var callback = () => api.sendMessage({body: `${name} à 💌, 𝐈 𝐥𝐨𝐯𝐞 𝐘𝐨𝐮 💗` , 
   attachment: fs.createReadStream(__dirname + "/cache/omngu.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/omngu.gif"));
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/omngu.gif")).on("close",() => callback());
-   };
+}
