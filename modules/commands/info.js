045f7cd10e59f438b7a2e289c904ab64f0f33c60
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-12 04:20:41 +0700

    Update info.js

--- modules/commands/info.js
+++ modules/commands/info.js
@@ -1,17 +1,17 @@
 module.exports.config = {
-  name: "info",
-  version: "2.1.0",
-  hasPermssion: 0,
-  credits: "Hung Cho (Khánh Milo Fix) mod thêm by TrúcCute",
-  description: "Xem thông tin thread/user",
-  commandCategory: "Tiện ích",
-  usages: "[thread/user]",
-  cooldowns: 5,
-  dependencies: {
-    "axios": "",
-    "fs-extra": "",
-    "request": ""
-  }
+    name: "info",
+    version: "2.1.0",
+    hasPermssion: 0,
+    credits: "Hung Cho (Khánh Milo Fix) mod thêm by TrúcCute",
+    description: "Xem thông tin thread/user",
+    commandCategory: "Bổ não",
+    usages: "[thread/user]",
+    cooldowns: 5,
+    dependencies: {
+        "axios": "",
+        "fs-extra": "",
+        "request": ""
+    }
 };
 
 const totalPath = __dirname + '/cache/totalChat.json';
@@ -20,134 +20,131 @@ const fs = require("fs-extra");
 const request = require("request");
 const axios = require("axios");
 
-module.exports.handleEvent = async ({
-  api,
-  event,
-  args
-}) => {
-  if (!fs.existsSync(totalPath)) fs.writeFileSync(totalPath, JSON.stringify({}));
-  let totalChat = JSON.parse(fs.readFileSync(totalPath));
-  if (!totalChat[event.threadID]) return;
-  if (Date.now() - totalChat[event.threadID].time > (_24hours * 2)) {
-    let sl = (await api.getThreadInfo(event.threadID)).messageCount;
-    totalChat[event.threadID] = {
-      time: Date.now() - _24hours,
-      count: sl,
-      ytd: sl - totalChat[event.threadID].count
-    }
-    fs.writeFileSync(totalPath, JSON.stringify(totalChat, null, 2));
-  }
-}
-
-module.exports.run = async function ({ api, event, args, Users}) {
-  const { threadID, messageID, senderID, type, mentions } = event;
-  const moment = require("moment-timezone");
-  const gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss")
-  if (args.length == 0) return api.sendMessage(`Vui lòng dùng\n=> ${global.config.PREFIX}${this.config.name} thread\n=> ${global.config.PREFIX}${this.config.name} user`, threadID);
-
-  if (args[0] == "thread") {
-    try {
-      if (!fs.existsSync(totalPath)) fs.writeFileSync(totalPath, JSON.stringify({}));
-      let totalChat = JSON.parse(fs.readFileSync(totalPath));
-      let threadInfo = await api.getThreadInfo(args[1] || threadID);
-      let timeByMS = Date.now();
-      var memLength = threadInfo.participantIDs.length;
-      let threadMem = threadInfo.participantIDs.length;
-      var nameMen = [];
-      var gendernam = [];
-      var gendernu = [];
-      var nope = [];
-      for (let z in threadInfo.userInfo) {
-      var gioitinhone = threadInfo.userInfo[z].gender;
-      var nName = threadInfo.userInfo[z].name;
-      if (gioitinhone == "MALE") {
-        gendernam.push(z + gioitinhone)
-      } else if (gioitinhone == "FEMALE") {
-        gendernu.push(gioitinhone)
-      } else {
-        nope.push(nName)
-      }
-    };
-      var nam = gendernam.length;
-      var nu = gendernu.length;
-      let qtv = threadInfo.adminIDs.length;
-      let sl = threadInfo.messageCount;
-      let u = threadInfo.nicknames;
-      let icon = threadInfo.emoji;
-      let threadName = threadInfo.threadName;
-      let id = threadInfo.threadID;
-      let sex = threadInfo.approvalMode;
-      var pd = sex == false ? '𝐭𝐚̆́𝐭' : sex == true ? '𝐛𝐚̣̂𝐭' : '𝐤𝐡';
-      if (!totalChat[event.threadID]) {
-      totalChat[event.threadID] = {
-        time: timeByMS,
-        count: sl,
-        ytd: 0
-      }
-      fs.writeFileSync(totalPath, JSON.stringify(totalChat, null, 2));
-      }
-      let mdtt = "𝐜𝐡𝐮̛𝐚 𝐜𝐨́ 𝐭𝐡𝐨̂́𝐧𝐠 𝐤𝐞̂";
-      let preCount = totalChat[event.threadID].count || 0;
-      let ytd = totalChat[event.threadID].ytd || 0;
-      let hnay = (ytd != 0) ? (sl - preCount) : "𝐜𝐡𝐮̛𝐚 𝐜𝐨́ 𝐭𝐡𝐨̂́𝐧𝐠 𝐤𝐞̂";
-      let hqua = (ytd != 0) ? ytd : "𝐜𝐡𝐮̛𝐚 𝐜𝐨́ 𝐭𝐡𝐨̂́𝐧𝐠 𝐤𝐞̂";
-      if (timeByMS - totalChat[event.threadID].time > _24hours) {
-        if (timeByMS - totalChat[event.threadID].time > (_24hours * 2)) {
-        totalChat[event.threadID].count = sl;
-        totalChat[event.threadID].time = timeByMS - _24hours;
-        totalChat[event.threadID].ytd = sl - preCount;
+module.exports.handleEvent = async ({ api, event, args }) => {
+    if (!fs.existsSync(totalPath)) fs.writeFileSync(totalPath, JSON.stringify({}));
+    let totalChat = JSON.parse(fs.readFileSync(totalPath));
+    if (!totalChat[event.threadID]) return;
+    if (Date.now() - totalChat[event.threadID].time > (_24hours * 2)) {
+        let sl = (await api.getThreadInfo(event.threadID)).messageCount;
+        totalChat[event.threadID] = {
+            time: Date.now() - _24hours,
+            count: sl,
+            ytd: sl - totalChat[event.threadID].count
+        }
         fs.writeFileSync(totalPath, JSON.stringify(totalChat, null, 2));
-      }
-      getHour = Math.ceil((timeByMS - totalChat[event.threadID].time - _24hours) / 3600000);
-        if (ytd == 0) mdtt = 100;
-        else mdtt = ((((hnay) / ((hqua / 24) * getHour))) * 100).toFixed(0);
-      mdtt += "%";
     }
-
-      var callback = () =>
-      api.sendMessage({
-        body: `⭐️𝐁𝐨𝐱: ${threadName}\n🎮 𝐈𝐃 𝐁𝐨𝐱: ${id}\n📱 𝐏𝐡𝐞̂ 𝐝𝐮𝐲𝐞̣̂𝐭: ${pd}\n🐰 𝐄𝐦𝐨𝐣𝐢: ${icon || "👍"}\n📌 𝐓𝐡𝐨̂𝐧𝐠 𝐭𝐢𝐧: ${threadMem} 𝐭𝐡𝐚̀𝐧𝐡 𝐯𝐢𝐞̂𝐧\n𝐒𝐨̂́ 𝐭𝐯 𝐧𝐚𝐦 🧑‍🦰: ${nam} 𝐭𝐡𝐚̀𝐧𝐡 𝐯𝐢𝐞̂𝐧\n𝐒𝐨̂́ 𝐭𝐯 𝐧𝐮̛̃ 👩‍🦰: ${nu} 𝐭𝐡𝐚̀𝐧𝐡 𝐯𝐢𝐞̂𝐧\n🕵️‍♂️ 𝐆𝐨̂̀𝐦 ${qtv} 𝐪𝐮𝐚̉𝐧 𝐭𝐫𝐢̣ 𝐯𝐢𝐞̂𝐧\n💬 𝐓𝐨̂̉𝐧𝐠: ${sl} 𝐭𝐢𝐧 𝐧𝐡𝐚̆́𝐧\n📈 𝐌𝐮̛́𝐜 𝐭𝐮̛𝐨̛𝐧𝐠 𝐭𝐚́𝐜: ${mdtt}\n🌟 𝐓𝐨̂̉𝐧𝐠 𝐬𝐨̂́ 𝐭𝐢𝐧 𝐧𝐡𝐚̆́𝐧 𝐡𝐨̂𝐦 𝐪𝐮𝐚: ${hqua}\n🌟 𝐓𝐨̂̉𝐧𝐠 𝐬𝐨̂́ 𝐭𝐢𝐧 𝐧𝐡𝐚̆́𝐧 𝐡𝐨̂𝐦 𝐧𝐚𝐲: ${hnay}\n⠀⠀⠀ ⠀ ⠀ 『${gio}』`,
-        attachment: fs.createReadStream(__dirname + '/cache/1.png')
-      },
-        event.threadID,
-        () => fs.unlinkSync(__dirname + '/cache/1.png'),
-        event.messageID
-      );
-    return request(encodeURI(`${threadInfo.imageSrc}`))
-      .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
-      .on('close', () => callback());
-    } catch (e) {
-  return ( 
-    console.log(e), 
-    api.sendMessage( 'Không thể lấy thông tin nhóm của bạn!', event.threadID, event.messageID )
-  )
-    }
-  }
-
-  if (args[0] == "user") {
-    try {
-      if (type == "message_reply") {
-        uid = event.messageReply.senderID
-    } else if (args.join().indexOf('@') !== -1) {
-        var uid = Object.keys(mentions)[0]
-    } else {
-        var uid = senderID
-  } 
-     let data = await api.getUserInfo(uid),
-       { profileUrl, gender, isFriend } = data[uid];
-    let name = await Users.getNameUser(uid)
-    var callback = () => api.sendMessage({body:
-      `💦Tên: `+ name +
-      `\n🐧UID: `+ uid +
-      `\n🙆‍♀️Trạng thái: ` + (isFriend == true ? "đã kết bạn với bot" : isFriend == false ? "chưa kết bạn với bot" : "UNKOWN") +
-      `\n🦋Giới tính: `+ (gender == 2 ? 'nam' : gender == 1 ? 'nữ' : 'UNKNOWN') +
-      `\n🏝Profile:\n`+ profileUrl,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"), messageID);   
-       return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=750&width=750&access_token=1073911769817594|aa417da57f9e260d1ac1ec4530b417de`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
-    } catch (e) {
-      return ( 
-        console.log(e), 
-        api.sendMessage( 'Không thể lấy thông tin người dùng!', event.threadID, event.messageID ) 
-      )
-    }
-  }
 }
+
+module.exports.run = async function({ api, event, args, Users }) {
+    const { threadID, messageID, senderID, type, mentions, messageReply } = event;
+    const moment = require("moment-timezone");
+    const gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss")
+    if (args.length == 0) return api.sendMessage(`Vui lòng dùng\n=> ${global.config.PREFIX}${this.config.name} thread\n=> ${global.config.PREFIX}${this.config.name} user`, threadID);
+    if (args[0] == "thread") {
+        try {
+            if (!fs.existsSync(totalPath)) fs.writeFileSync(totalPath, JSON.stringify({}));
+            let totalChat = JSON.parse(fs.readFileSync(totalPath));
+            let threadInfo = await api.getThreadInfo(args[1] || threadID);
+            let timeByMS = Date.now();
+            var memLength = threadInfo.participantIDs.length;
+            let threadMem = threadInfo.participantIDs.length;
+            var nameMen = [];
+            var gendernam = [];
+            var gendernu = [];
+            var nope = [];
+            for (let z in threadInfo.userInfo) {
+                var gioitinhone = threadInfo.userInfo[z].gender;
+                var nName = threadInfo.userInfo[z].name;
+                if (gioitinhone == "MALE") {
+                    gendernam.push(z + gioitinhone)
+                } else if (gioitinhone == "FEMALE") {
+                  gendernu.push(gioitinhone)
+                } else {
+                    nope.push(nName)
+                }
+            };
+            var nam = gendernam.length;
+            var nu = gendernu.length;
+            let qtv = threadInfo.adminIDs.length;
+            let sl = threadInfo.messageCount;
+            let u = threadInfo.nicknames;
+            let icon = threadInfo.emoji;
+            let threadName = threadInfo.threadName;
+            let id = threadInfo.threadID;
+            let sex = threadInfo.approvalMode;
+            var pd = sex == false ? '𝐭𝐚̆́𝐭' : sex == true ? '𝐛𝐚̣̂𝐭' : '𝐤𝐡';
+            if (!totalChat[args[1] || threadID]) {
+              totalChat[args[1] || threadID] = {
+                    time: timeByMS,
+                    count: sl,
+                    ytd: 0
+                }
+              fs.writeFileSync(totalPath, JSON.stringify(totalChat, null, 2));
+            }
+            let mdtt = "𝐜𝐡𝐮̛𝐚 𝐜𝐨́ 𝐭𝐡𝐨̂́𝐧𝐠 𝐤𝐞̂";
+            let preCount = totalChat[args[1] || threadID].count || 0;
+            let ytd = totalChat[args[1] || threadID].ytd || 0;
+            let hnay = (ytd != 0) ? (sl - preCount) : "𝐜𝐡𝐮̛𝐚 𝐜𝐨́ 𝐭𝐡𝐨̂́𝐧𝐠 𝐤𝐞̂";
+            let hqua = (ytd != 0) ? ytd : "𝐜𝐡𝐮̛𝐚 𝐜𝐨́ 𝐭𝐡𝐨̂́𝐧𝐠 𝐤𝐞̂";
+            if (timeByMS - totalChat[args[1] || threadID].time > _24hours) {
+                if (timeByMS - totalChat[args[1] || threadID].time > (_24hours * 2)) {
+                  totalChat[args[1] || threadID].count = sl;
+                  totalChat[args[1] || threadID].time = timeByMS - _24hours;
+                  totalChat[args[1] || threadID].ytd = sl - preCount;
+                  fs.writeFileSync(totalPath, JSON.stringify(totalChat, null, 2));
+                }
+                getHour = Math.ceil((timeByMS - totalChat[args[1] || event.threadID].time - _24hours) / 3600000);
+                if (ytd == 0) mdtt = 100;
+                else mdtt = ((((hnay) / ((hqua / 24) * getHour))) * 100).toFixed(0);
+                mdtt += "%";
+            }
+            var callback = () =>
+                api.sendMessage({
+                        body: `⭐️𝐁𝐨𝐱: ${threadName}\n🎮 𝐈𝐃 𝐁𝐨𝐱: ${id}\n📱 𝐏𝐡𝐞̂ 𝐝𝐮𝐲𝐞̣̂𝐭: ${pd}\n🐰 𝐄𝐦𝐨𝐣𝐢: ${icon || "👍"}\n📌 𝐓𝐡𝐨̂𝐧𝐠 𝐭𝐢𝐧: ${threadMem} 𝐭𝐡𝐚̀𝐧𝐡 𝐯𝐢𝐞̂𝐧\n𝐒𝐨̂́ 𝐭𝐯 𝐧𝐚𝐦 🧑‍🦰: ${nam} 𝐭𝐡𝐚̀𝐧𝐡 𝐯𝐢𝐞̂𝐧\n𝐒𝐨̂́ 𝐭𝐯 𝐧𝐮̛̃ 👩‍🦰: ${nu} 𝐭𝐡𝐚̀𝐧𝐡 𝐯𝐢𝐞̂𝐧\n🕵️‍♂️ 𝐆𝐨̂̀𝐦 ${qtv} 𝐪𝐮𝐚̉𝐧 𝐭𝐫𝐢̣ 𝐯𝐢𝐞̂𝐧\n💬 𝐓𝐨̂̉𝐧𝐠: ${sl} 𝐭𝐢𝐧 𝐧𝐡𝐚̆́𝐧\n📈 𝐌𝐮̛́𝐜 𝐭𝐮̛𝐨̛𝐧𝐠 𝐭𝐚́𝐜: ${mdtt}\n🌟 𝐓𝐨̂̉𝐧𝐠 𝐬𝐨̂́ 𝐭𝐢𝐧 𝐧𝐡𝐚̆́𝐧 𝐡𝐨̂𝐦 𝐪𝐮𝐚: ${hqua}\n🌟 𝐓𝐨̂̉𝐧𝐠 𝐬𝐨̂́ 𝐭𝐢𝐧 𝐧𝐡𝐚̆́𝐧 𝐡𝐨̂𝐦 𝐧𝐚𝐲: ${hnay}\n⠀⠀⠀ ⠀ ⠀ 『${gio}』`,
+                        attachment: fs.createReadStream(__dirname + '/cache/1.png')
+                    },
+                    threadID,
+                    () => fs.unlinkSync(__dirname + '/cache/1.png'),
+                    messageID
+                );
+            return request(encodeURI(`${threadInfo.imageSrc}`))
+              .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
+                .on('close', () => callback());
+        } catch (e) {
+            return (
+              console.log(e),
+              api.sendMessage('Không thể lấy thông tin nhóm của bạn!', threadID, messageID)
+            )
+        }
+    }
+    if (args[0] == "user") {
+        try {
+            if (type == "message_reply") {
+                uid = messageReply.senderID
+            } else if (args.join().indexOf('@') !== -1) {
+                var uid = Object.keys(mentions)[0]
+            } else {
+                var uid = senderID
+            }
+            let data = await api.getUserInfo(uid),
+                { profileUrl, gender, isFriend } = data[uid];
+            let name = await Users.getNameUser(uid)
+            var callback = () => 
+              api.sendMessage({
+                body:
+                    `💦Tên: ` + name +
+                    `\n🐧UID: ` + uid +
+                    `\n🙆‍♀️Trạng thái: ` + (isFriend == true ? "đã kết bạn với bot" : isFriend == false ? "chưa kết bạn với bot" : "UNKOWN") +
+                    `\n🦋Giới tính: ` + (gender == 2 ? 'nam' : gender == 1 ? 'nữ' : 'UNKNOWN') +
+                    `\n🏝Profile:\n` + profileUrl,
+                attachment: fs.createReadStream(__dirname + "/cache/1.png")
+            }, threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"), messageID);
+            return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=750&width=750&access_token=1073911769817594|aa417da57f9e260d1ac1ec4530b417de`)).pipe(fs.createWriteStream(__dirname + '/cache/1.png')).on('close', () => callback());
+        } catch (e) {
+            return (
+              console.log(e),
+              api.sendMessage('Không thể lấy thông tin người dùng!', threadID, messageID)
+            )
+        }
+    }
+  }
