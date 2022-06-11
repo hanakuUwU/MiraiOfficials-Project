686cb3ce049b7228194ebec44bf3ca1c02e13179
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-11 13:48:38 +0700

    Update setdatabox.js

--- modules/commands/setdatabox.js
+++ modules/commands/setdatabox.js
@@ -19,10 +19,10 @@ var inbox = await api.getThreadList(100, null, ['INBOX']);
   let list = [...inbox].filter(group => group.isSubscribed && group.isGroup);
   const lengthGroup = list.length
   for (var groupInfo of list) {
-    console.log(chalk[colors[Math.floor(Math.random() * colors.length)]](`Đã cập nhật dữ liệu của box ID: ${groupInfo.threadID}`))
     var threadInfo = await api.getThreadInfo(groupInfo.threadID);
     threadInfo.threadName;
     await Threads.setData(groupInfo.threadID, { threadInfo });
+    console.log(chalk[colors[Math.floor(Math.random() * colors.length)]](`Đã cập nhật dữ liệu của ${threadInfo.threadName} || ${groupInfo.threadID}`))
   }
     console.log(chalk[colors[Math.floor(Math.random() * colors.length)]](`Đã cập nhật dữ liệu của ${lengthGroup} box`))
     return api.sendMessage(`Đã cập nhật dữ liệu của ${lengthGroup} box`, threadID)
