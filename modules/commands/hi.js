3995be351dd529fba3a60b87336f351ea909d32c
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-14 02:09:58 +0700

    Update hi.js

--- modules/commands/hi.js
+++ modules/commands/hi.js
@@ -1,31 +1,32 @@
 module.exports.config = {
     name: "hi",
-    version: "1.0.1",
+    version: "1.0.2",
     hasPermssion: 0,
-    credits: "Kanichi",
+    credits: "Kanichi mod time by TrúcCute",
     description: "",
     commandCategory: "không cần dấu lệnh",
     usages: "",
     cooldowns: 0,
     denpendencies: {
         "fs": "",
-        "request": ""
+        "request": "",
+        "moment-timezone": ""
     }
 };
 module.exports.onLoad = () => {
     const fs = require("fs-extra");
     const request = require("request");
+    const moment = require("moment-timezone");
     const dirMaterial = __dirname + `/noprefix/`;
     if (!fs.existsSync(dirMaterial + "noprefix")) fs.mkdirSync(dirMaterial, { recursive: true });
     if (!fs.existsSync(dirMaterial + "hi.gif")) request("https://i.imgur.com/fr6s1TW.gif").pipe(fs.createWriteStream(dirMaterial + "hi.gif"));
 }
 module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils, global, client }) => {
     const fs = require("fs");
-    let name = await Users.getNameUser(event.senderID)
-    var msg = {
-                body: `𝐇𝐢 𝐁𝐚̣𝐧 😉 ${name}`,
-                attachment: fs.createReadStream(__dirname + `/noprefix/hi.gif`)
-            }
+    const hours = moment.tz('Asia/Ho_Chi_Minh').format('HH');
+  let session = hours <= 10 ? "sáng" : hours > 10 && hours <= 12 ? "trưa" : hours > 12 && hours <= 18 ? "chiều" : "tối";
+  let name = await Users.getNameUser(event.senderID)
+	 var msg = {body: `chào buổi ${session}, chúc ${name} 1 ngày vui vẻ`, attachment: fs.createReadStream(__dirname + `/noprefix/hi.gif`)}
     if (event.body.toLowerCase() == "hi"){
         return api.sendMessage(msg,event.threadID,event.messageID);}
     if (event.body.toLowerCase() == "hii"){
@@ -35,6 +36,10 @@ module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils,
     if (event.body.toLowerCase() == "hì"){
         return api.sendMessage(msg,event.threadID,event.messageID);}
     if (event.body.toLowerCase() == "híí"){
+        return api.sendMessage(msg,event.threadID,event.messageID);}
+    if (event.body.toLowerCase() == "hello"){
+        return api.sendMessage(msg,event.threadID,event.messageID);}
+    if (event.body.toLowerCase() == "chào"){
         return api.sendMessage(msg,event.threadID,event.messageID);}
         };
 module.exports.run = async ({ event, api, Currencies, args, utils }) => {
