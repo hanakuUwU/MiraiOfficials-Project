dceb385aac765729ee9a3e6d37e3bd1991afebff
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-14 22:20:30 +0700

    Update setname.js

--- modules/commands/setname.js
+++ modules/commands/setname.js
@@ -1,17 +1,23 @@
 module.exports.config = {
 	name: "setname",
-	version: "1.0.0",
+	version: "1.0.2",
 	hasPermssion: 0,
-	credits: "Mirai Team",
+	credits: "Mirai Team mod reply by TrúcCute",
 	description: "Đổi biệt danh trong nhóm của bạn hoặc của người bạn tag",
-	commandCategory: "other",
-	usages: "[name]",
-	cooldowns: 3
-};
+	commandCategory: "Bổ não",
+	usages: "[trống/tag/reply] + [name]",
+	cooldowns: 5
+}
 
-module.exports.run = async function({ api, event, args }) {
-	const name = args.join(" ")
+module.exports.run = async ({ api, event, args }) => {
+	if (event.type == "message_reply") {
+    const name = args.join(" ")
+    return api.changeNickname(`${name}`, event.threadID, event.messageReply.senderID);
+  }
+  else {
+    const name = args.join(" ")
 	const mention = Object.keys(event.mentions)[0];
 	if (!mention) return api.changeNickname(`${name}`, event.threadID, event.senderID);
 	if (mention[0]) return api.changeNickname(`${name.replace(event.mentions[mention], "")}`, event.threadID, mention);
+  }
 }
