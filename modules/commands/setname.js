d9ec08ba56715f5f4658d102ec42a6cb8404c3ab
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-16 23:35:21 +0700

    Update setname.js

--- modules/commands/setname.js
+++ modules/commands/setname.js
@@ -1,23 +1,37 @@
 module.exports.config = {
 	name: "setname",
-	version: "1.0.2",
+	version: "1.1.0",
 	hasPermssion: 0,
-	credits: "Mirai Team mod reply by TrúcCute",
+	credits: "TrúcCute",
 	description: "Đổi biệt danh trong nhóm của bạn hoặc của người bạn tag",
 	commandCategory: "Bổ não",
 	usages: "[trống/tag/reply] + [name]",
 	cooldowns: 5
 }
 
-module.exports.run = async ({ api, event, args }) => {
-	if (event.type == "message_reply") {
+module.exports.run = async ({ api, event, args, Users }) => {
+  let { threadID, messageReply, senderID, mentions, type } = event;
+  const delayUnsend = 60;// tính theo giây
+	if (type == "message_reply") {
+    let name2 = await Users.getNameUser(messageReply.senderID)
     const name = args.join(" ")
-    return api.changeNickname(`${name}`, event.threadID, event.messageReply.senderID);
+    return api.changeNickname(`${name}`, threadID, messageReply.senderID),
+      api.sendMessage(`Đã đổi tên của ${name2} thành ${name || "tên gốc"}`, threadID, (err, info) =>
+	setTimeout(() => {api.unsendMessage(info.messageID) }, delayUnsend * 1000))
   }
   else {
+	const mention = Object.keys(mentions)[0];
+	const name2 = await Users.getNameUser(mention || senderID)
+    if (args.join().indexOf('@') !== -1 ) {
+      const name = args.join(' ')
+      return api.changeNickname(`${name.replace(mentions[mention],"")}`, threadID, mention),
+        api.sendMessage(`Đã đổi tên của ${name2} thành ${name.replace(mentions[mention],"") || "tên gốc"}`, threadID, (err, info) =>
+          setTimeout(() => {api.unsendMessage(info.messageID) } , delayUnsend * 1000))
+      } else {
     const name = args.join(" ")
-	const mention = Object.keys(event.mentions)[0];
-	if (!mention) return api.changeNickname(`${name}`, event.threadID, event.senderID);
-	if (mention[0]) return api.changeNickname(`${name.replace(event.mentions[mention], "")}`, event.threadID, mention);
+      return api.changeNickname(`${name}`, threadID, senderID),
+        api.sendMessage(`Đã đổi tên của ${name2} thành ${name || "tên gốc"}`, threadID, (err, info) =>
+          setTimeout(() => {api.unsendMessage(info.messageID) } , delayUnsend * 1000))
+      }
+    }
   }
-}
