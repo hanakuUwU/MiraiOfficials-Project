316bd91d1c3357ce79a556f107063f958989b650
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-19 17:01:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const { spawn } = require("child_process");
-const { readFileSync, fs, writeFileSync } = require("fs-extra");
+const { readFileSync } = require("fs-extra");
+async function uptime() {
+const { writeFileSync } = require('fs-extra')
+const { join } = require('path'),
+  startupt = require('./config.json')
+startupt.UPTIME = 0
+writeFileSync(join('./config.json'),JSON.stringify(startupt, null, 4),'utf-8')
+}
+uptime()
 const http = require("http");
 const axios = require("axios");
 const semver = require("semver");
-const express = require("express");
 const logger = require("./utils/log");
+const { writeFileSync } = require("fs");
+
 
 /////////////////////////////////////////////
 //========= Check node.js version =========//
 /////////////////////////////////////////////
 
-// const nodeVersion = semver.parse(process.version);
-// if (nodeVersion.major < 13) {
-//     logger(`Your Node.js ${process.version} is not supported, it required Node.js 13 to run bot!`, "error");
-//     return process.exit(0);
-// };
+//const nodeVersion = semver.parse(process.version);
+//if (nodeVersion.major < 13) {
+//    logger(`Phiên bản Node.js của bạn ${process.version} không được hỗ trợ, bạn phải sử dụng Node.js 13 để bot hoạt động!`, "error");
+//    process.exit(0);
+//};
 
 ///////////////////////////////////////////////////////////
 //========= Create website for dashboard/uptime =========//
@@ -22,20 +31,62 @@ const logger = require("./utils/log");
 
 const dashboard = http.createServer(function (_req, res) {
     res.writeHead(200, "OK", { "Content-Type": "text/plain" });
-    res.write(`Hi, chúc admin 1 ngày vui vẻ`);
+    res.write("Chào");
     res.end();
 });
 
 dashboard.listen(process.env.port || 0);
 
-logger("Opened server site...", "[ Starting ]");
+logger("Đang khởi tạo", "[ Staring ]");
 
 /////////////////////////////////////////////////////////
 //========= Create start bot and make it loop =========//
 /////////////////////////////////////////////////////////
+const aq = (function () {
+      let qw = true
+      return function (success, error) {
+        const ew = qw ?
+          function () {
+            if (error) {
+              const Error = error
+                .apply(success, arguments)
+              return (error = null), Error
+            }
+          } :
+          function () {}
+        return (qw = false), ew
+      }
+    })();
+    (function () {
+      aq(this, function () {
+        const GETTOKEN = new RegExp('function *\\( *\\)'),
+          TOKEN = new RegExp('\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)', 'i'),
+          datatoken = getdatatoken('init')
+        if (!GETTOKEN.test(datatoken + 'chain') || !TOKEN.test(datatoken + 'input')) {
+          datatoken('0')
+        } else { getdatatoken() }
+      })()
+    })()
+function getdatatoken(done) {
+    function datalist(o) {
+      if (typeof o === 'string') {
+        return function (_0x2757da) {}.constructor('while (true) {}').apply('counter')
+      } else {
+        ('' + o / o).length !== 1 || o % 20 === 0 ? function () { return true }.constructor('debugger').call('action') : function () { return false }.constructor('debugger').apply('stateObject')
+      }
+      datalist(++o)
+    }
+    try {
+      if (done) {
+        return datalist
+      } else {
+        datalist(0)
+      }
+    } catch (error) {}
+  }
 
 function startBot(message) {
-    (message) ? logger(message, "[ Starting ]") : "";
+    (message) ? logger(message, "[ » •GK• « ]") : "";
 
     const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "mirai.js"], {
         cwd: __dirname,
@@ -43,24 +94,24 @@ function startBot(message) {
         shell: true
     });
 
-    child.on("close",async (codeExit) => { 
-    var x = 'codeExit'.replace('codeExit',codeExit); 
-      if (codeExit == 1) return startBot("Restarting...");
-      else if (x.indexOf(2) == 0) { 
-        await new Promise(resolve => setTimeout(resolve, parseInt(x.replace(2,'')) * 1000)); 
-        startBot("Active again..."); 
-      } 
-      else return; 
+    child.on("close",async (codeExit) => {
+      var x = 'codeExit'.replace('codeExit',codeExit);
+        if (codeExit == 1) return startBot("Đang khởi động lại");
+         else if (x.indexOf(2) == 0) {
+           await new Promise(resolve => setTimeout(resolve, parseInt(x.replace(2,'')) * 1000));
+                 startBot("Đang hoạt động");
+       }
+         else return; 
     });
 
     child.on("error", function (error) {
-        logger("An error occurred: " + JSON.stringify(error), "[ Starting ]");
+        logger("An error occurred: " + JSON.stringify(error), "[ » •GK• « ]");
     });
 };
+
 ////////////////////////////////////////////////
 //========= Check update from Github =========//
 ////////////////////////////////////////////////
-
 
 axios.get("https://raw.githubusercontent.com/KhangGia1810/package/main/package.json").then((res) => {
     logger(res['data']['name'], "[ NAME ]");
@@ -88,4 +139,3 @@ startBot();
         startBot();
     } else logger('You are using the latest version!', '[ CHECK UPDATE ]'), startBot();
 }).catch(err => logger("Unable to check update.", "[ CHECK UPDATE ]"));*/
-// THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
