900e165db928a62ee098c6b5e0f7ddff37049946
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-21 23:43:39 +0700

    Update love.js

--- modules/commands/love.js
+++ modules/commands/love.js
@@ -9,7 +9,7 @@ module.exports.config = {
   cooldowns: 1
 }
 module.exports.run = async ({ api, event,args, Users }) => {
-  var token = `6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+  var token = `566034311867544|uYVSZPr3ktHpAsv11gWUkz9D4JI`;
   const axios = global.nodemodule["axios"];
   const fs = global.nodemodule["fs-extra"];
   if (!args[0]) {
