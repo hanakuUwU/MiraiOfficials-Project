a023f3ade0e9aa7151f550241bbdb49315f92d6c
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-22 22:05:00 +0700

    Update ghép.js

--- modules/commands/ghép.js
+++ modules/commands/ghép.js
@@ -26,7 +26,7 @@ module.exports.onLoad = async() => {
 }
 
 async function makeImage({ one, two }) {
-  var token = `566034311867544|uYVSZPr3ktHpAsv11gWUkz9D4JI`;
+  var token = `6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
     const path = require("path");
     const axios = require("axios"); 
     const jimp = require("jimp");
