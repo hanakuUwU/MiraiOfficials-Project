eb6575034f5f937b34abdedd16126fc2ab5b97de
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-22 22:05:26 +0700

    Update tile.js

--- modules/commands/tile.js
+++ modules/commands/tile.js
@@ -15,7 +15,7 @@ module.exports.config = {
   }
 
 module.exports.run = async ({ api, event, Users, args }) => {
-  var token = `566034311867544|uYVSZPr3ktHpAsv11gWUkz9D4JI`;
+  var token = `6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
   let axios = require('axios');
   let fs = require('fs-extra');
   let request = require('request');
