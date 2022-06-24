0dd0aaa58b1051ae60d0187a501fcf187e0233f4
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-24 19:04:37 +0700

    Update setprefix.js

--- modules/commands/setprefix.js
+++ modules/commands/setprefix.js
@@ -1,13 +1,38 @@
 module.exports.config = {
-	name: "setprefix",
-	version: "1.0.1",
-	hasPermssion: 1,
-	credits: "Mirai Team",
-	description: "Đặt lại prefix của nhóm",
-	commandCategory: "config",
-	usages: "[prefix/reset]",
-	cooldowns: 5
-};
+  name: "setprefix",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "ManhG + setprefix của Mirai Team",
+  description: "Xem prefix của BOT",
+  commandCategory: "noprefix",
+  usages: "",
+  cooldowns: 5
+}
+
+module.exports.handleEvent = async ({ event, api, Threads }) => {
+  var { threadID, messageID, body, senderID } = event;
+  //if (senderID == global.data.botID) return;
+  if ((this.config.credits) != "ManhG") { return api.sendMessage(`Sai credits!`, threadID, messageID)}
+  function out(data) {
+    api.sendMessage(data, threadID, messageID)
+  }
+  var dataThread = (await Threads.getData(threadID));
+  var data = dataThread.data; 
+  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+
+  var arr = ["mpre","mprefix","prefix", "dấu lệnh", "prefix của bot là gì","daulenh"];
+  arr.forEach(i => {
+    let str = i[0].toUpperCase() + i.slice(1);
+    if (body === i.toUpperCase() | body === i | str === body) {
+		const prefix = threadSetting.PREFIX || global.config.PREFIX;
+      if (data.PREFIX == null) {
+        return out(`『 ${prefix} 』Nhóm chưa xét prefix mới cho bot`)
+      }
+      else return out('Prefix của nhóm là: ' + data.PREFIX)
+    }
+
+  })
+}
 
 module.exports.languages ={
 	"vi": {
