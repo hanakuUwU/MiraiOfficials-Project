a4152156526ab14ae124664b26bb6f1e1257ec93
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-25 16:25:14 +0700

    Update dovui.js

--- modules/commands/dovui.js
+++ modules/commands/dovui.js
@@ -8,7 +8,8 @@ module.exports.config = {
     commandCategory: "game",
     usages: "",
     cooldowns: 5
-};
+}
+
 module.exports.handleReply = async function ({ args, event, Users, api, handleReply }) {
     const axios = global.nodemodule['axios'];  
     var { dataGame, dapan, nameUser } = handleReply;
@@ -27,12 +28,12 @@ module.exports.handleReply = async function ({ args, event, Users, api, handleRe
                 api.unsendMessage(handleReply.messageID)
                 var msg = {body: `✔${nameUser} đã trả lời chính xác!\nĐáp án: B\n${checkTrue}`}
                 return api.sendMessage(msg, event.threadID, event.messageID)
-            }else 
+            } else 
              if (aw.toLowerCase() == 'c' && dataGame.c == checkTrue) {
                 api.unsendMessage(handleReply.messageID)
                 var msg = {body: `✔${nameUser} đã trả lời chính xác!\nĐáp án: C\n${checkTrue}`}
                 return api.sendMessage(msg, event.threadID, event.messageID)
-            }else 
+            } else 
              if (aw.toLowerCase() == 'd' && dataGame.d == checkTrue) {
                 api.unsendMessage(handleReply.messageID)
                 var msg = {body: `✔${nameUser} đã trả lời chính xác!\nĐáp án: D\n${checkTrue}`}
@@ -41,43 +42,40 @@ module.exports.handleReply = async function ({ args, event, Users, api, handleRe
             else {
             api.unsendMessage(handleReply.messageID)
             api.sendMessage(`✘Tiếc quá! ${nameUser} trả lời sai rồi!!!\nĐáp án chính xác là: ${dapan}`, event.threadID);
-        }
+            }
         }
     }
 }
+
 module.exports.handleReaction = ({ Users, api, event, handleReaction,  }) => {
     var { dataGame, dapan, author, nameUser } = handleReaction;
     //if (parseInt(event.userID) !== parseInt(handleReaction.author)) return;
-    
     if (event.userID != author) return;
     console.log(event.userID)
     console.log(author)
-    if (event.reaction != "👍" && event.reaction != "😆"&& event.reaction != "😮"&& event.reaction != "❤️") return;
+    if (event.reaction != "👍" && event.reaction != "😆"&& event.reaction != "😮"&& event.reaction != "😀") return;
     let response = "";
     if (event.reaction == "👍") response = dataGame.a
-    else if (event.reaction == "❤️") response = dataGame.b
+    else if (event.reaction == "😀") response = dataGame.b
     else if (event.reaction == "😆") response = dataGame.c
     else if (event.reaction == "😮") response = dataGame.d
 
     if (response == handleReaction.dapan) { 
         api.unsendMessage(handleReaction.messageID)
         api.sendMessage(`✔Hay quá! ${nameUser} trả lời đúng rồi.\nĐáp án: ${dapan}`, event.threadID) 
-    }
-
-    else {
+    } else {
     api.unsendMessage(handleReaction.messageID)
     api.sendMessage(`✘Tiếc quá! ${nameUser} trả lời sai rồi!!!\nĐáp án chính xác là: ${dapan}`, event.threadID);
-}
-
+    }
 }
 
 module.exports.run = async function ({ api, event, Users }) {
 const axios = global.nodemodule['axios'];  
 const { threadID, messageID } = event;
-const res = await axios.get(`http://api.leanhtruong.net/api/dovui?apikey=leanhtruong_FP9YR0cFSJWtdcKcQaKy`); // res key tại leanhtruong.net 
+const res = await axios.get(`https://apidovui.miraiofficials123.repl.co/`);
 const dataGame = res.data
 var namePlayer_react = await Users.getData(event.senderID)
-        var msg = {body: `❔Câu hỏi dành cho bạn: ${dataGame.questions}\n\n👍/A. ${dataGame.a}\n❤️/B. ${dataGame.b}\n😆/C. ${dataGame.c}\n😮/D. ${dataGame.d}\n\n🌻Reply tin nhắn hoặc thả cảm xúc để trả lời`}
+        var msg = {body: `❔Câu hỏi dành cho bạn: ${dataGame.questions}\n\n👍/A. ${dataGame.a}\n😀/B. ${dataGame.b}\n😆/C. ${dataGame.c}\n😮/D. ${dataGame.d}\n\n🌻Reply tin nhắn hoặc thả cảm xúc để trả lời`}
         return api.sendMessage(msg, event.threadID, (error, info) => {
         global.client.handleReaction.push({
             type: "reply",
