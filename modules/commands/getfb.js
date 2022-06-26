a5329a40d328a32bd779479f431a0a96908bdc9c
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-06-26 14:16:21 +0700

    Update getfb.js

--- modules/commands/getfb.js
+++ modules/commands/getfb.js
@@ -5,18 +5,22 @@ module.exports.config = {
   credits: "TrúcCute",//chính chủ xin đừng hiểu nhầm thay credits 
   description: "Lấy link fb",
   commandCategory: "tiện ích",
-  usages: "[trống] or [tag]/[reply]",
+  usages: "[trống] or [tag]/[reply]/[uid]",
   cooldowns: 5
 }
-module.exports.run = async function({ api, event, args}) {
+module.exports.run = async function ({ api, event, args }) {
   const { messageReply, senderID, threadID, messageID, type, mentions} = event;
-  if (type == "message_reply") {
+  if (!args[0]) {
+    if (type == "message_reply") {
       uid = messageReply.senderID
-    } else if (args.join().indexOf('@') !== -1) {
-        var uid = Object.keys(mentions)[0]
-    }  else {
-        var uid = senderID
-  } 
+    } else {
+      uid = senderID
+    }
+  } else if (args.join().indexOf('@') !== -1) {
+      uid = Object.keys(mentions)[0]
+  } else {
+    uid = args[0]
+  }
   let data = await api.getUserInfo(uid),
        { profileUrl } = data[uid];
   return api.sendMessage(`${profileUrl}`, threadID, messageID)
