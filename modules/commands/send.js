821a8259a2fdc9169b3a7553c7819eeca83cd30d
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-07-04 14:29:59 +0700

    Update send.js

--- modules/commands/send.js
+++ modules/commands/send.js
@@ -1,13 +1,19 @@
 module.exports.config = {
 	name: "send",
-	version: "1.0.2",
+	version: "1.0.5",
 	hasPermssion: 2,
-	credits: "Mirai mod by HĐGN",
+	credits: "Mirai mod by HĐGN mod lại by TrúcCute",
 	description: "Gửi tin nhắn tới các nhóm(reply vào ảnh/video cần gửi kèm)!\nPhiên bản xịn hơn của sendnotiUwU",
 	commandCategory: "Admin",
 	usages: "[Text]",
-	cooldowns: 5
-};
+	cooldowns: 5,
+  dependencies: {
+    "fs": "",
+    "axios": "",
+    "moment-timezone": "",
+    "request": ""
+  }
+}
 
 module.exports.languages = {
 	"vi": {
@@ -21,56 +27,58 @@ module.exports.languages = {
 }
 
 module.exports.run = async ({ api, event, args, getText, Users }) => {
-  const name = await Users.getNameUser(event.senderID)
-const moment = require("moment-timezone");
-      var gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss");  
-if (event.type == "message_reply") {
-const request = global.nodemodule["request"];
-const fs = require('fs')
-const axios = require('axios')
-			var getURL = await request.get(event.messageReply.attachments[0].url);
-			
-					var pathname = getURL.uri.pathname;
-var ext = pathname.substring(pathname.lastIndexOf(".") + 1);
-			
-					var path = __dirname + `/cache/snoti`+`.${ext}`;
-
-
-var abc = event.messageReply.attachments[0].url;
-    let getdata = (await axios.get(`${abc}`, { responseType: 'arraybuffer' })).data;
-
-  fs.writeFileSync(path, Buffer.from(getdata, 'utf-8'));
-
-
-	var allThread = global.data.allThreadID || [];
-	var count = 1,
-		cantSend = [];
-	for (const idThread of allThread) {
-		if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
-		else {
-			api.sendMessage({body: `ㅤ »🌸 𝑨𝑫𝑴𝑰𝑵 𝑩𝑶𝑻 🌸«\n\n𝐓𝐡𝐨̛̀𝐢 𝐠𝐢𝐚𝐧: ${gio}\n𝐆𝐮̛̉𝐢 𝐭𝐮̛̀ 𝐀𝐝𝐦𝐢𝐧: ${name}\n𝐍𝐨̣̂𝐢 𝐝𝐮𝐧𝐠:\n『 ${args.join(` `)} 』`,attachment: fs.createReadStream(path) }, idThread, (error, info) => {
-				if (error) cantSend.push(idThread);
-			});
-			count++;
-			await new Promise(resolve => setTimeout(resolve, 500));
-		}
-	}
-	return api.sendMessage(getText("sendSuccess", count), event.threadID, () => (cantSend.length > 0 ) ? api.sendMessage(getText("sendFail", cantSend.length), event.threadID, event.messageID) : "", event.messageID);
-
-}
-else {
-	var allThread = global.data.allThreadID || [];
-	var count = 1,
-		cantSend = [];
-	for (const idThread of allThread) {
-		if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
-		else {
-			api.sendMessage(`====== [ 𝐓𝐡𝐨̂𝐧𝐠 𝐁𝐚́𝐨 ] ======\n\n𝐓𝐡𝐨̛̀𝐢 𝐠𝐢𝐚𝐧: ${gio}\n𝐆𝐮̛̉𝐢 𝐭𝐮̛̀ 𝐀𝐝𝐦𝐢𝐧: ${name}\n𝐍𝐨̣̂𝐢 𝐝𝐮𝐧𝐠:\n『 ${args.join(` `)} 』`, idThread, (error, info) => {
-				if (error) cantSend.push(idThread);
-			});
-			count++;
-			await new Promise(resolve => setTimeout(resolve, 500));
-		}
-	}
-	return api.sendMessage(getText("sendSuccess", count), event.threadID, () => (cantSend.length > 0 ) ? api.sendMessage(getText("sendFail", cantSend.length), event.threadID, event.messageID) : "", event.messageID); }
+  let { senderID, messageReply, threadID, messageID, type } = event;
+  let name = await Users.getNameUser(senderID)
+  let moment = require("moment-timezone")
+  let gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss")
+  if (type == "message_reply") {
+    let request = require("request")
+    let fs = require('fs')
+    let axios = require('axios')
+    if (messageReply.attachments[0].type == "audio") {
+      path = __dirname + `/cache/snoti.m4a` ||  __dirname + `/cache/snoti.mp3`
+    }
+    if (messageReply.attachments[0].type == "photo") {
+      path = __dirname + `/cache/snoti.jpg`
+    }
+    if (messageReply.attachments[0].type == "video") {
+      path = __dirname + `/cache/snoti.mp4`
+    }
+    if (messageReply.attachments[0].type == "animated_image") {
+      path = __dirname + `/cache/snoti.gif`
+    }
+    let abc = messageReply.attachments[0].url;
+    let getdata = (await axios.get(`${abc}`, {
+      responseType: 'arraybuffer'
+    })).data
+    fs.writeFileSync(path, Buffer.from(getdata, 'utf-8'))
+    let allThread = global.data.allThreadID || []
+    let count = 1, cantSend = [];
+    for (idThread of allThread) {
+      if (isNaN(parseInt(idThread)) || idThread == threadID) ""
+      else {
+        api.sendMessage({body: `ㅤ »🌸 𝑨𝑫𝑴𝑰𝑵 𝑩𝑶𝑻 🌸«\n\n𝐓𝐡𝐨̛̀𝐢 𝐠𝐢𝐚𝐧: ${gio}\n𝐆𝐮̛̉𝐢 𝐭𝐮̛̀ 𝐀𝐝𝐦𝐢𝐧: ${name}\n𝐍𝐨̣̂𝐢 𝐝𝐮𝐧𝐠:\n『 ${args.join(` `)} 』`, attachment: fs.createReadStream(path) }, idThread, (e, info) => {
+          if (e) cantSend.push(idThread);
+        });
+        count++;
+      await new Promise(resolve => setTimeout(resolve, 500));
+      }
+    }
+    return api.sendMessage(getText("sendSuccess", count), threadID, () => (cantSend.length > 0 ) ? api.sendMessage(getText("sendFail", cantSend.length), threadID, messageID) : "", messageID)
   }
+  else {
+    let allThread = global.data.allThreadID || [];
+    let count = 1, cantSend = [];
+    for (idThread of allThread) {
+      if (isNaN(parseInt(idThread)) || idThread == threadID) ""
+      else {
+        api.sendMessage(`====== [ 𝐓𝐡𝐨̂𝐧𝐠 𝐁𝐚́𝐨 ] ======\n\n𝐓𝐡𝐨̛̀𝐢 𝐠𝐢𝐚𝐧: ${gio}\n𝐆𝐮̛̉𝐢 𝐭𝐮̛̀ 𝐀𝐝𝐦𝐢𝐧: ${name}\n𝐍𝐨̣̂𝐢 𝐝𝐮𝐧𝐠:\n『 ${args.join(` `)} 』`, idThread, (error, info) => {
+          if (error) cantSend.push(idThread)
+        });
+        count++;
+        await new Promise(resolve => setTimeout(resolve, 500))
+      }
+    }
+    return api.sendMessage(getText("sendSuccess", count), threadID, () => (cantSend.length > 0 ) ? api.sendMessage(getText("sendFail", cantSend.length), threadID, messageID) : "", messageID)
+  }
+}
